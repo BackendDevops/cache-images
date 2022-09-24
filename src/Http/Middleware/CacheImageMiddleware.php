db53d6b0ea76d968233f0fa7e336e5c5c264fc6a
Kıvanç İnci <kivanc.inci@kabipartners.com>
2022-09-25 02:16:10 +0300

    middleware change according to config changes

--- src/Http/Middleware/CacheImageMiddleware.php
+++ src/Http/Middleware/CacheImageMiddleware.php
@@ -31,7 +31,7 @@ class CacheImageMiddleware
                 'filter' => $filter,
                 'filename' => $filename
             ] =   $request->route()->parameters();
-            $module = '';
+            $module = 'default';
         }
 
         if (!str_contains($filename,'_')){
@@ -39,20 +39,14 @@ class CacheImageMiddleware
         }
         $file = explode('_',$filename);
         $filename = $file[0];
-        $extension = $file[1];
-        if ($module==""){
-            $is_exists = File::exists(public_path('uploads/'.$filter."/".$filename."webp"));
-        }else{
-            $is_exists = File::exists(public_path('uploads/'.$module."/".$filter."/".$filename."webp"));
-        }
-        if ($is_exists){
-            if ($module==""){
-                $img = public_path('uploads/'.$filter."/".$filename."webp");
+        $config = config('cache-image');
+        $path_to_check = $config['mapping'][$module].'/'.$filter.'/'.$filename."webp";
 
-            }else{
-                $img = public_path('uploads/'.$module."/".$filter."/".$filename."webp");
-            }
-            $response = Response::make($img);
+        $is_exists = File::exists(public_path($path_to_check));
+
+        if ($is_exists){
+            $image = Image::make($path_to_check);
+            $response = Response::make($image);
             $response->header('Content-Type', 'image/webp');
             return $response;
         }
