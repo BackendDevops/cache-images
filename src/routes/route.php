fc88e9d8e2bb0b6b90f28a184047b13036594adb
Kıvanç İnci <kivanc.inci@kabipartners.com>
2022-09-26 01:35:12 +0300

    Update route.php

--- src/routes/route.php
+++ src/routes/route.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Kvnc\CacheImages\Http\Controllers\ImageCacheController;
 Route::get('image-repo/{filter}/{filename}', [ImageCacheController::class,'withoutModule'])
-    ->name('kvnc_image_cache.cache')->middleware('image-cache');
-Route::get('image-repo/{filter}/{module}//{filename}', [ImageCacheController::class,'index'])
+    ->name('kvnc_image_cache.cache.no_module')->middleware('image-cache');
+Route::get('image-repo/{filter}/{module}/{filename}', [ImageCacheController::class,'index'])
     ->name('kvnc_image_cache.cache')->middleware('image-cache');
