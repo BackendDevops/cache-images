aa31b06af4be23b6574bd740d93fd50d21c50b66
Kıvanç İnci <kivanc.inci@kabipartners.com>
2022-09-26 00:57:04 +0300

    Update ImageResizeService.php

--- src/Services/ImageResizeService.php
+++ src/Services/ImageResizeService.php
@@ -12,7 +12,6 @@ class ImageResizeService
     protected array $medium = ['width'=> 600 ,'height' => 400];
     protected array $large = ['width' => 1280, 'height' => 900];
     protected  array $config;
-    public Intervention\Image\Image $image;
 
     public function __construct()
     {
@@ -30,30 +29,30 @@ class ImageResizeService
             if (str_contains($filename,'_')){
                 $file = explode('_',$filename);
                 File::ensureDirectoryExists(public_path('uploads/'.$module.'/'.$filter));
-                $this->image = Image::make(public_path('uploads/'.$module.'/originals/'.$file[0].'.'.$file[1]));
+                $image = Image::make(public_path('uploads/'.$module.'/originals/'.$file[0].'.'.$file[1]));
 
                return $this->process($filter,$module,$file)->response('webp',100);
             }
 
         }
 
-        protected function process( string $filter, string $module, array $file): Image
+        protected function process( \Intervention\Image\Image $image,string $filter, string $module, array $file): Image
         {
-            if ($this->image->width() >= $this->image->height()){
-                $this->image->resize($this->config['filters'][$filter]['width'],null,function ($constraint){
+            if ($image->width() >= $image->height()){
+                $image->resize($this->config['filters'][$filter]['width'],null,function ($constraint){
                     $constraint->aspectRatio();
                     $constraint->upsize();
                 });
             }
-            if ($this->image->width() < $this->image->height()){
-                $this->image->resize(null,$this->config['filters'][$filter]['width'],function ($constraint){
+            if ($image->width() < $image->height()){
+                $image->resize(null,$this->config['filters'][$filter]['width'],function ($constraint){
                     $constraint->aspectRatio();
                     $constraint->upsize();
                 });
             }
             $path = $this->config['mapping'][$module]."/".$filter."/".implode('.',$file);
-            $this->image->save(public_path($path));
-            return $this->image;
+            $image->save(public_path($path));
+            return $image;
         }
 
 
